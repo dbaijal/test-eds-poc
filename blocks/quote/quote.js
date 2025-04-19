6d849824b90548fe2f59e07e9fc0b6977da485cd
Deepti <deepti.baijal@gmail.com>
2025-04-19 12:01:23 +0530

    code for Article Cards component

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,14 @@
+import { fetchPlaceholders } from '../../scripts/aem.js';
+
+const globalProperties = await fetchPlaceholders();
+console.log("placeholder Object is " + globalProperties.blockquote);
 export default function decorate(block) {
     const [quoteWrapper] = block.children;
-  
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
+    // Append additional content from globalProperties.blockquote
+    const placeholderContent = document.createTextNode("~~" + globalProperties.blockquote);
+    blockquote.appendChild(placeholderContent);
     quoteWrapper.replaceChildren(blockquote);
   }
   
