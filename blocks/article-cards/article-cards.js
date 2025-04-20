1fbb02225605bd68c02b74d65aa0faa0895ab6fa
Deepti <deepti.baijal@gmail.com>
2025-04-21 00:03:16 +0530

    code for article component

--- blocks/article-cards/article-cards.js
+++ blocks/article-cards/article-cards.js
@@ -1,3 +1,18 @@
+import { div } from '../../scripts/dom-helpers.js';
+
 export default function decorate(block) {
-  
+  const [heading, ...cards] = [...block.children];
+  if (heading) {
+    heading.classList.add('article-cards-heading');
+  }
+  const articleCardsContainer = div({ class: 'article-card-container' });
+  cards.forEach(async (row) => {
+    console.log("row is "+ row);
+    row.className = 'article-card';
+    const [imageDiv,titleDiv] = row.children;
+    imageDiv.className = 'mini-card-image';
+    titleDiv.className = 'mini-card-text';
+    articleCardsContainer.append(row);
+  });
+  block.append(articleCardsContainer);
 }
