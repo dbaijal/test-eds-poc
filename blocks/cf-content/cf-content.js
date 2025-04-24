ae84bff600c04791b8e6fb14869ba41d7ca31f72
Deepti <deepti.baijal@gmail.com>
2025-04-24 22:58:39 +0530

    code

--- blocks/cf-content/cf-content.js
+++ blocks/cf-content/cf-content.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-    const aempublishurl = 'https://publish-p99952-e1559416.adobeaemcloud.com';
+    const aempublishurl = 'https://author-p99952-e1559416.adobeaemcloud.com';
     const persistedquery = '/graphql/execute.json/aem-eds-poc/articleInformation';
     const contentPath = '/content/dam/aem-eds-poc/content-fragments/article-1';
     const url = `${aempublishurl}${persistedquery};path=${contentPath}`;
