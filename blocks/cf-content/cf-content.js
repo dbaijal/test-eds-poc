6c8f3ba696d73fc6ca45b23e334756a54d465b09
Deepti <deepti.baijal@gmail.com>
2025-04-25 13:37:42 +0530

    updated content path

--- blocks/cf-content/cf-content.js
+++ blocks/cf-content/cf-content.js
@@ -1,7 +1,8 @@
 export default async function decorate(block) {
     const aempublishurl = 'https://author-p118103-e1621695.adobeaemcloud.com';
     const persistedquery = '/graphql/execute.json/aem-eds-poc/articleInformation';
-    const contentPath = '/content/dam/aem-eds-poc/content-fragment/article1';
+    //const contentPath = '/content/dam/aem-eds-poc/content-fragment/article1';
+    const contentPath = block.querySelector(':scope div:nth-child(2) > div a')?.textContent?.trim();
     const url = `${aempublishurl}${persistedquery};path=${contentPath}`;
     const options = { credentials: 'include'};
     const cfReq = await fetch(url, options)
