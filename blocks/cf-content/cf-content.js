0ab7b23f2f9aa52abd421572b4e0902e2aff126d
Deepti <deepti.baijal@gmail.com>
2025-04-25 13:16:58 +0530

    updated value for aem instance

--- blocks/cf-content/cf-content.js
+++ blocks/cf-content/cf-content.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
-    const aempublishurl = 'https://author-p99952-e1559416.adobeaemcloud.com';
+    const aempublishurl = 'https://author-p118103-e1621695.adobeaemcloud.com';
     const persistedquery = '/graphql/execute.json/aem-eds-poc/articleInformation';
-    const contentPath = '/content/dam/aem-eds-poc/content-fragments/article-1';
+    const contentPath = '/content/dam/aem-eds-poc/content-fragment/article1';
     const url = `${aempublishurl}${persistedquery};path=${contentPath}`;
     const options = { credentials: 'include'};
     const cfReq = await fetch(url, options)
