bc97566c0afb0842b15bbdfd9b9b4a9d84cbfada
Deepti <deepti.baijal@gmail.com>
2025-04-25 14:13:45 +0530

    content fragment block content updated

--- blocks/cf-content/cf-content.js
+++ blocks/cf-content/cf-content.js
@@ -1,4 +1,4 @@
-export default async function decorate(block) {
+/*export default async function decorate(block) {
     const aempublishurl = 'https://author-p118103-e1621695.adobeaemcloud.com';
     const persistedquery = '/graphql/execute.json/aem-eds-poc/articleInformation';
     //const contentPath = '/content/dam/aem-eds-poc/content-fragment/article1';
@@ -16,4 +16,39 @@ export default async function decorate(block) {
   block.innerHTML = `${
     cfReq?.articleItem
   }`;
-}
+} */
+
+  export default async function decorate(block) {
+    const aempublishurl = 'https://author-p118103-e1621695.adobeaemcloud.com';
+    const persistedquery = '/graphql/execute.json/aem-eds-poc/articleInformation';
+    
+    // Get the content fragment path from the block (2nd child div)
+    const contentPath = block.querySelector(':scope div:nth-child(2) > div a')?.textContent?.trim();
+  
+    const url = `${aempublishurl}${persistedquery};path=${contentPath}`;
+    const options = { credentials: 'include' };
+  
+    const cfReq = await fetch(url, options)
+      .then((response) => response.json())
+      .then((contentfragment) => {
+        return contentfragment?.data?.articleinfoByPath?.item || null;
+      });
+  
+    // Clear existing block content
+    block.innerHTML = '';
+  
+    // Only render if data is available
+    if (cfReq && contentPath) {
+      const itemId = `urn:aemconnection:${contentPath}/jcr:content/data/master`;
+  
+      block.innerHTML = `
+        <div data-aue-resource="${itemId}" data-aue-type="reference" data-aue-filter="cf">
+          <p data-aue-prop="articleItem" data-aue-label="Article Item" data-aue-type="text">${cfReq?.articleItem || ''}</p>
+          <p data-aue-prop="articleAuthor" data-aue-label="Article Author" data-aue-type="text">${cfReq?.articleAuthor || ''}</p>
+        </div>
+      `;
+    } else {
+      block.innerHTML = `<p>Failed to load content fragment.</p>`;
+    }
+  }
+  
